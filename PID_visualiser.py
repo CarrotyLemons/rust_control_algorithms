76af252528da4e918a39d75a4fdc23cef2f07ea5
CarrotyLemons <david@CarrotyLemons.local>
2025-04-19 03:42:00 +0930

    Initial commit

---
+++ PID_visualiser.py
@@ -0,0 +1,25 @@
+import csv
+import matplotlib.pyplot as plt
+import os
+
+csv_path = "PID_simulation.csv"
+
+with open(csv_path) as file:
+    reader = csv.reader(file)
+    next(reader)
+    data = list(reader)
+
+time = [float(row[0]) for row in data]
+target = [float(row[1]) for row in data]
+measured = [float(row[2]) for row in data]
+
+plt.plot(time, measured, label = "Measured")
+plt.plot(time, target, label = "Target", linestyle = ':')
+plt.xlabel("Time (s)")
+plt.ylabel("Value")
+plt.title("PID rust simulation output")
+plt.legend()
+plt.grid()
+plt.show()
+
+os.remove(csv_path)
---
+++ README.md
@@ -0,0 +1,6 @@
+Hello!
+This is a rust library implementing a simple PID controller.
+
+It includes some non-extensive unit tests and one output of a PID running process. This process involves the control of acceleration to try and reach a position.
+
+The `PID_visualiser.py` can be run on the output file to show the PID display of the example case in the file.
